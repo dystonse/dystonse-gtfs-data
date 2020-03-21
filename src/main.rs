f75eb44cbfb126773fb48224860bef74039399ea
Lena Schimmel <lena@serioese.gmbh>
2020-03-21 20:09:02 +0100

    Use gtfs-structure instead of manual csv parsing.

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,16 @@
 use prost::Message; // need to use this, otherwise FeedMessage won't have a `decode` method
-use serde::Deserialize;
-use std::collections::HashMap;
 use std::env;
 use std::error::Error;
 use std::ffi::OsString;
 use std::fs::File;
 use std::io::prelude::*;
 use std::process;
+use gtfs_structures::Gtfs;
 
 use gtfs_rt::FeedMessage;
 
-#[derive(Debug, Deserialize)]
-struct Route {
-    route_id: String,
-    agency_id: u32,
-    route_short_name: String,
-    route_type: u32,
-    // Don't read fields that we won't need anyway
-    // route_long_name: String,
-    // route_color: String,
-    // route_text_color: String,
-    // route_desc: String
-}
-
-/// Reads the csv file at the given path and puts each Route it reads into routes, using the route_id as the key.
-fn read_csv(file_path: OsString, routes: &mut HashMap<String, Route>) -> Result<(), Box<dyn Error>> {
-    let file = File::open(file_path)?;
-    let mut rdr = csv::Reader::from_reader(file);
-
-    for result in rdr.deserialize() {
-        // we need to put this into a var because the compiler needs a type annotation for Route
-        let route: Route = result?;
-
-        // we clone route.route_id because we can't pass its ownership to the map, 
-        // and references don't work either for some reson 
-        routes.insert(route.route_id.clone(), route);
-    }
-
-    Ok(())
-}
-
 /// Reads the pb file at the given path and prints each trip update, usind some data from the routes
-fn read_pb(file_path: OsString, routes: &mut HashMap<String, Route>) -> Result<(), Box<dyn Error>> {
+fn read_pb(file_path: OsString, gtfs: & Gtfs) -> Result<(), Box<dyn Error>> {
     let mut file = File::open(file_path)?;
     let mut vec = Vec::<u8>::new();
 
@@ -55,7 +24,7 @@ fn read_pb(file_path: OsString, routes: &mut HashMap<String, Route>) -> Result<(
             // the fields of trip_update are Options, so we need to handle the case that they are missing.
             let route_id = trip_update.trip.route_id.expect("Trip needs route_id");
             let start_time = trip_update.trip.start_time.expect("Trip needs start_time");
-            println!("Trip update for Route {:?} at {}", routes.get(&route_id).expect("Route nicht gefunden").route_short_name, start_time);
+            println!("Trip update for Route {:?} at {}", gtfs.get_route(&route_id).expect("Route nicht gefunden").short_name, start_time);
         }
     }
     Ok(())
@@ -71,10 +40,9 @@ fn get_nth_arg(n: usize) -> Result<OsString, Box<dyn Error>> {
 }
 
 fn real_main() -> Result<(), Box<dyn Error>> {
-    let mut routes: HashMap<String, Route> = HashMap::new();
-
-    read_csv(get_nth_arg(1)?, &mut routes)?;
-    read_pb(get_nth_arg(2)?, &mut routes)?;
+    let gtfs = Gtfs::new(get_nth_arg(1)?.to_str().expect("gtfs filename")).expect("Gtfs deserialisierung");
+    //let gtfs = Gtfs::new(get_nth_arg(1)?.as_os_str().to_str().expect("Blöder String"));
+    read_pb(get_nth_arg(2)?, &gtfs)?;
     Ok(())
 }
 
