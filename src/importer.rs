c29b93329dee6b77c4d68bfc38a359ebb8dd40c2
Lena Schimmel <mail@lenaschimmel.de>
2020-03-25 01:38:00 +0100

    Code formatting.

--- src/importer.rs
+++ src/importer.rs
@@ -1,12 +1,12 @@
+use chrono::{NaiveDate, NaiveDateTime, NaiveTime, Timelike};
+use gtfs_rt::FeedMessage as GtfsRealtimeMessage;
 use gtfs_structures::Gtfs;
+use gtfs_structures::Trip as ScheduleTrip;
+use mysql::prelude::*;
 use mysql::*;
 use prost::Message; // need to use this, otherwise GtfsRealtimeMessage won't have a `decode` method
-use mysql::prelude::*;
 use std::fs::File;
 use std::io::prelude::*;
-use chrono::{NaiveDate,NaiveDateTime,NaiveTime,Timelike};
-use gtfs_rt::FeedMessage as GtfsRealtimeMessage;
-use gtfs_structures::Trip as ScheduleTrip;
 
 use crate::FnResult;
 
@@ -15,41 +15,48 @@ const MAX_BATCH_SIZE: usize = 1000;
 pub struct Importer<'a> {
     pool: &'a Pool,
     gtfs_schedule: &'a Gtfs,
-    verbose: bool
+    verbose: bool,
 }
 
 enum EventType {
     Arrival,
-    Departure
+    Departure,
 }
 
 struct EventTimes {
     schedule: Option<i64>,
     estimate: Option<i64>,
-    delay: Option<i64>
+    delay: Option<i64>,
 }
 
 impl EventTimes {
     fn empty() -> EventTimes {
-        EventTimes {schedule: None, estimate: None, delay: None }
+        EventTimes {
+            schedule: None,
+            estimate: None,
+            delay: None,
+        }
     }
 }
 
 struct BatchedInsertions<'a> {
     params_vec: Vec<Params>,
     conn: &'a mut PooledConn,
-    statement: Statement
+    statement: Statement,
 }
 
 impl<'a> Importer<'a> {
     pub fn new(gtfs_schedule: &'a Gtfs, pool: &'a Pool, verbose: bool) -> FnResult<Importer<'a>> {
-        Ok(Importer { gtfs_schedule, pool, verbose})
+        Ok(Importer {
+            gtfs_schedule,
+            pool,
+            verbose,
+        })
     }
 
-    pub fn import_realtime_into_database(& self, path: &str) -> FnResult<()> {
+    pub fn import_realtime_into_database(&self, path: &str) -> FnResult<()> {
         let mut file = File::open(path)?;
         let mut vec = Vec::<u8>::new();
-        
         if path.ends_with(".zip") {
             let mut archive = zip::ZipArchive::new(file).unwrap();
             let mut zipped_file = archive.by_index(0).unwrap();
@@ -60,10 +67,8 @@ impl<'a> Importer<'a> {
         } else {
             file.read_to_end(&mut vec)?;
         }
-    
         // suboptimal, I'd rather not read the whole file into memory, but maybe Prost just works like this
         let message = GtfsRealtimeMessage::decode(&vec)?;
-    
         let time_of_recording = message.header.timestamp.expect("No global timestamp");
 
         // TODO: Remove those statistics, they aren't accurate anyway
@@ -75,33 +80,32 @@ impl<'a> Importer<'a> {
 
         let mut conn = self.pool.get_conn()?;
         let mut batched = BatchedInsertions::new(&mut conn);
-        
         // `message.entity` is actually a collection of entities
         for entity in message.entity {
             if let Some(trip_update) = entity.trip_update {
                 count_all_trip_updates += 1;
 
                 let realtime_trip = trip_update.trip;
-    
                 let route_id = realtime_trip.route_id.expect("Trip needs route_id");
                 let trip_id = realtime_trip.trip_id.expect("Trip needs id");
 
                 let start_date = if let Some(datestring) = realtime_trip.start_date {
-                    NaiveDate::parse_from_str(&datestring, "%Y%m%d").expect(&datestring).and_hms(0, 0, 0)
+                    NaiveDate::parse_from_str(&datestring, "%Y%m%d")
+                        .expect(&datestring)
+                        .and_hms(0, 0, 0)
                 } else {
-                    
                     eprintln!("Trip without start date. Skipping.");
                     continue;
                 };
 
                 // TODO check if we actually need this
-                let realtime_schedule_start_time = if let Some(timestring) = realtime_trip.start_time {
-                    NaiveTime::parse_from_str(&timestring, "%H:%M:%S").expect(&timestring)
-                } else {
-                    eprintln!("Trip without start time. Skipping.");
-                    continue;
-                };
-                
+                let realtime_schedule_start_time =
+                    if let Some(timestring) = realtime_trip.start_time {
+                        NaiveTime::parse_from_str(&timestring, "%H:%M:%S").expect(&timestring)
+                    } else {
+                        eprintln!("Trip without start time. Skipping.");
+                        continue;
+                    };
                 let schedule_trip = if let Ok(trip) = self.gtfs_schedule.get_trip(&trip_id) {
                     trip
                 } else {
@@ -110,38 +114,50 @@ impl<'a> Importer<'a> {
                 };
 
                 let schedule_start_time = schedule_trip.stop_times[0].departure_time;
-                let time_difference = realtime_schedule_start_time.num_seconds_from_midnight() - schedule_start_time.unwrap();
+                let time_difference = realtime_schedule_start_time.num_seconds_from_midnight()
+                    - schedule_start_time.unwrap();
                 if time_difference != 0 {
                     eprintln!("Trip {} has a difference of {} seconds between scheduled start times in schedule data and realtime data.", trip_id, time_difference);
                 }
-    
 
                 for stop_time_update in trip_update.stop_time_update {
                     count_all_stop_time_updates += 1;
 
                     let stop_id = stop_time_update.stop_id.expect("no stop_id");
-                    let stop_sequence = stop_time_update.stop_sequence.expect("no stop_sequence") as usize;
-    
+                    let stop_sequence =
+                        stop_time_update.stop_sequence.expect("no stop_sequence") as usize;
                     // There's an enum but conversion to u32 is not supported: gtfs.get_route(&route_id).expect("I've got no route!!!").route_type as u32;
                     // TODO add a method impl to RouteType to convert it back to u32
                     let mode = 99;
 
-                    let arrival   = Importer::handle_stop_time_update(stop_time_update.arrival,   start_date, EventType::Arrival  , &schedule_trip, stop_sequence);
-                    let departure = Importer::handle_stop_time_update(stop_time_update.departure, start_date, EventType::Departure, &schedule_trip, stop_sequence);
+                    let arrival = Importer::handle_stop_time_update(
+                        stop_time_update.arrival,
+                        start_date,
+                        EventType::Arrival,
+                        &schedule_trip,
+                        stop_sequence,
+                    );
+                    let departure = Importer::handle_stop_time_update(
+                        stop_time_update.departure,
+                        start_date,
+                        EventType::Departure,
+                        &schedule_trip,
+                        stop_sequence,
+                    );
 
-                    batched.add_insertion(Params::from(params! { 
-                        "trip_id" => &trip_id, 
+                    batched.add_insertion(Params::from(params! {
+                        "trip_id" => &trip_id,
                         stop_id,
                         "route_id" => &route_id,
-                        stop_sequence, 
-                        time_of_recording, 
-                        mode, 
-                        "time_arrival_schedule" => arrival.schedule, 
-                        "time_arrival_estimate" => arrival.estimate, 
+                        stop_sequence,
+                        time_of_recording,
+                        mode,
+                        "time_arrival_schedule" => arrival.schedule,
+                        "time_arrival_estimate" => arrival.estimate,
                         "delay_arrival" => arrival.delay,
-                        "time_departure_schedule" => departure.schedule, 
-                        "time_departure_estimate" => departure.estimate, 
-                        "delay_departure" => departure.delay 
+                        "time_departure_schedule" => departure.schedule,
+                        "time_departure_estimate" => departure.estimate,
+                        "delay_departure" => departure.delay
                     }))?;
 
                     count_success += 1;
@@ -156,17 +172,24 @@ impl<'a> Importer<'a> {
         Ok(())
     }
 
-    fn handle_stop_time_update(event: Option<gtfs_rt::trip_update::StopTimeEvent>, 
-                               start_date: NaiveDateTime, 
-                               event_type: EventType,
-                               schedule_trip: &ScheduleTrip,
-                               stop_sequence: usize
-                              ) -> EventTimes {
+    fn handle_stop_time_update(
+        event: Option<gtfs_rt::trip_update::StopTimeEvent>,
+        start_date: NaiveDateTime,
+        event_type: EventType,
+        schedule_trip: &ScheduleTrip,
+        stop_sequence: usize,
+    ) -> EventTimes {
         let delay = if let Some(event) = event {
             if let Some(delay) = event.delay {
                 delay as i64
             } else {
-                eprintln!("Stop time update {} without delay. Skipping.", match event_type { EventType::Arrival => "arrival", EventType::Departure => "departure" });
+                eprintln!(
+                    "Stop time update {} without delay. Skipping.",
+                    match event_type {
+                        EventType::Arrival => "arrival",
+                        EventType::Departure => "departure",
+                    }
+                );
                 return EventTimes::empty();
             }
         } else {
@@ -174,17 +197,21 @@ impl<'a> Importer<'a> {
         };
 
         let event_time = match event_type {
-            EventType::Arrival   => schedule_trip.stop_times[stop_sequence].arrival_time,
-            EventType::Departure => schedule_trip.stop_times[stop_sequence].departure_time
+            EventType::Arrival => schedule_trip.stop_times[stop_sequence].arrival_time,
+            EventType::Departure => schedule_trip.stop_times[stop_sequence].departure_time,
         };
         let schedule = start_date.timestamp() + event_time.expect("no arrival time") as i64;
         let estimate = schedule + delay;
-        
-        EventTimes { delay: Some(delay), schedule: Some(schedule), estimate: Some(estimate) }
+
+        EventTimes {
+            delay: Some(delay),
+            schedule: Some(schedule),
+            estimate: Some(estimate),
+        }
     }
 }
 
-impl <'a> BatchedInsertions<'a> {
+impl<'a> BatchedInsertions<'a> {
     fn new(conn: &mut PooledConn) -> BatchedInsertions {
         let statement = conn.prep(r"INSERT INTO `realtime` 
                     (`id`, `trip_id`, `stop_id`, `route_id`, `stop_sequence`, `mode`, `delay_arrival`, `delay_departure`,
@@ -193,11 +220,15 @@ impl <'a> BatchedInsertions<'a> {
                     (NULL, :trip_id, :stop_id, :route_id, :stop_sequence, :mode, :delay_arrival, :delay_departure, 
                     FROM_UNIXTIME(:time_of_recording), FROM_UNIXTIME(:time_arrival_schedule), FROM_UNIXTIME(:time_arrival_estimate), FROM_UNIXTIME(:time_departure_schedule), FROM_UNIXTIME(:time_departure_estimate))")
                     .expect("Could not prepare statement");
-    
-        BatchedInsertions { params_vec : Vec::with_capacity(MAX_BATCH_SIZE), conn, statement}
+
+        BatchedInsertions {
+            params_vec: Vec::with_capacity(MAX_BATCH_SIZE),
+            conn,
+            statement,
+        }
     }
 
-    fn add_insertion(&mut self, insertion: Params) -> FnResult<()>  {
+    fn add_insertion(&mut self, insertion: Params) -> FnResult<()> {
         self.params_vec.push(insertion);
         if self.params_vec.len() > MAX_BATCH_SIZE {
             self.write_to_database()?;
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use gtfs_structures::Gtfs;
 use std::env;
 use std::error::Error;
-use gtfs_structures::Gtfs;
 
 use mysql::*;
 use rayon::prelude::*;
@@ -8,38 +8,41 @@ use rayon::prelude::*;
 mod importer;
 use importer::Importer;
 
-// This is handy, because mysql defines its own Result type and we don't 
+// This is handy, because mysql defines its own Result type and we don't
 // want to repeat std::result::Result
 type FnResult<R> = std::result::Result<R, Box<dyn Error>>;
 
-/// Opens a connection to a database and returns the resulting connection pool. 
+/// Opens a connection to a database and returns the resulting connection pool.
 /// Takes configuration values from DB_PASSWORD, DB_USER, DB_HOST, DB_PORT and DB_DATABASE
 /// environment variables. For all values except DB_PASSWORD a default is provided.
-fn open_db() -> FnResult<Pool>  {
+fn open_db() -> FnResult<Pool> {
     let password = env::var("DB_PASSWORD")?;
     let user = env::var("DB_USER").unwrap_or(String::from("dystonse"));
     let host = env::var("DB_HOST").unwrap_or(String::from("localhost"));
     let port = env::var("DB_PORT").unwrap_or(String::from("3306"));
     let database = env::var("DB_DATABASE").unwrap_or(String::from("dystonse"));
-    let url = format!("mysql://{}:{}@{}:{}/{}", user, password, host, port, database);
+    let url = format!(
+        "mysql://{}:{}@{}:{}/{}",
+        user, password, host, port, database
+    );
     let pool = Pool::new(url)?;
-    
     Ok(pool)
 }
 
-/// Opens the database, reads schedule and transfers realtime data from 
+/// Opens the database, reads schedule and transfers realtime data from
 /// protobuffer files into the database.
 /// gtfs is read from the first command line parameter (url or path to zip or directory)
 /// gtfsrt is read from all other command line parameters (path to pb file)
 fn main() -> FnResult<()> {
-
     let mut args = env::args_os();
     // skip element 0 because it's the executable's name
-    args.next(); 
+    args.next();
 
     // use element 1 as schedule file
     let mut gtfs_schedule_filename_oss = args.next().unwrap();
-    let mut gtfs_schedule_filename = gtfs_schedule_filename_oss.to_str().expect("invalid OsString");
+    let mut gtfs_schedule_filename = gtfs_schedule_filename_oss
+        .to_str()
+        .expect("invalid OsString");
 
     let mut verbose = false;
 
@@ -47,11 +50,15 @@ fn main() -> FnResult<()> {
     if gtfs_schedule_filename == "-v" {
         verbose = true;
         gtfs_schedule_filename_oss = args.next().unwrap();
-        gtfs_schedule_filename = gtfs_schedule_filename_oss.to_str().expect("invalid OsString");
+        gtfs_schedule_filename = gtfs_schedule_filename_oss
+            .to_str()
+            .expect("invalid OsString");
     }
 
     // use all other elements as realtime file
-    let gtfs_realtime_filenames = args.map(|arg| String::from(arg.to_str().expect("invalid OsString")));
+    let gtfs_realtime_filenames: Vec<String> = args
+        .map(|arg| String::from(arg.to_str().expect("invalid OsString")))
+        .collect();
 
     // connect to the database
     if verbose {
@@ -91,11 +98,11 @@ fn process_gtfs_realtime(
     imp: &Importer,
     verbose: bool,
 ) -> FnResult<()> {
-        imp.import_realtime_into_database(&gtfs_realtime_filename)?;
-        if verbose {
-            println!("Finished importing file: {}", &gtfs_realtime_filename);
-        } else {
-            println!("{}", &gtfs_realtime_filename);
-        }
+    imp.import_realtime_into_database(&gtfs_realtime_filename)?;
+    if verbose {
+        println!("Finished importing file: {}", &gtfs_realtime_filename);
+    } else {
+        println!("{}", &gtfs_realtime_filename);
+    }
     Ok(())
 }
