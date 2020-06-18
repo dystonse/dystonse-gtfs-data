b1542b74b312867d13ffd34d57ac2eeac420e4fa
Lena Schimmel <mail@lenaschimmel.de>
2020-06-18 23:55:58 +0200

    Add PredictionResult.

---
+++ src/types/prediction_result.rs
@@ -0,0 +1,8 @@
+use dystonse_curves::{Curve, CurveSet, IrregularDynamicCurve};
+
+pub enum PredictionResult {
+    General(Box<dyn Curve>), // for the route type, independant of initial delay
+    SemiSpecific(Box<dyn Curve>), // for the specific route, route_variant and end_stop_id, but independant of initial delay
+    SpecificCurve(Box<dyn Curve>), // for the specific route, route_variant, {start and end}_stop_id and initial delay
+    SpecificCurveSet(CurveSet<f32, IrregularDynamicCurve<f32, f32>>), // for the specific route, route_variant, {start and end}_stop_id, but independant of initial delay
+}
