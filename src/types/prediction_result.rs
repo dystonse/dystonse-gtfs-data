4e687285f03a3738d0670ac7af0e25e4f80176f0
Kirstin Rohwer <mail@metakiki.net>
2020-06-24 16:30:22 +0200

    derive debug format for prediction results

--- src/types/prediction_result.rs
+++ src/types/prediction_result.rs
@@ -1,6 +1,7 @@
 use dystonse_curves::{Curve, CurveSet, IrregularDynamicCurve};
 
-#[allow(dead_code)]
+#[allow(dead_code)] 
+#[derive(Debug)]
 pub enum PredictionResult {
     General(Box<dyn Curve>), // for the route type, independant of initial delay
     SemiSpecific(Box<dyn Curve>), // for the specific route, route_variant and end_stop_id, but independant of initial delay
