80789a1aaf72e01c5256caac2c5d8361200b85c3
Lena Schimmel <mail@lenaschimmel.de>
2020-08-10 14:11:30 +0200

    Fix color escaping.

--- src/monitor/mod.rs
+++ src/monitor/mod.rs
@@ -26,15 +26,15 @@ use std::io::Write;
 use journey_data::{JourneyData, JourneyComponent, StopData, TripData};
 
 const CSS:&'static str = include_str!("style.css");
-const FAVICON_HEADERS: &'static str = r#"
+const FAVICON_HEADERS: &'static str = r##"
 <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png">
 <link rel="manifest" href="/favicons/site.webmanifest">
-<link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="\#5bbad5">
-<meta name="msapplication-TileColor" content="\#00aba9">
-<meta name="theme-color" content="\#ffffff">
-"#;
+<link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#00aba9">
+<meta name="theme-color" content="#ffffff">
+"##;
 
 #[derive(Clone)]
 pub struct Monitor {
